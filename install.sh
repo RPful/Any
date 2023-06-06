47c099c9b52f61ac00a9cf72d68cd2c4e2ea1d49
RPful <118921045+RPful@users.noreply.github.com>
2023-06-06 09:02:36 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ dpkg-reconfigure --frontend noninteractive tzdata > /dev/null 2>&1
 clear
 echo -e "\E[44;1;37m    INSTALAR CHECKUSER ANYVPN     \E[0m" 
 echo ""
-echo -e "                 \033[1;31mBy @LightXVD\033[1;36m"
+echo -e "                 \033[1;31mBy @RP4321\033[1;36m"
 echo ""
 echo -ne "\n\033[1;32mDE UM ENTER PARA \033[1;33mCONTINUAR...\033[1;37m: "; read -r
 clear
@@ -16,15 +16,15 @@ pip3 install flask > /dev/null 2>&1
 rm /bin/chany > /dev/null 2>&1
 sleep 5
 cd /bin || exit
-wget https://raw.githubusercontent.com/LightY4gami/checkany/main/chany > /dev/null 2>&1
-wget https://raw.githubusercontent.com/LightY4gami/checkany/main/userscheck > /dev/null 2>&1
+wget https://raw.githubusercontent.com/RPful/Any/main/chany > /dev/null 2>&1
+wget https://raw.githubusercontent.com/RPful/Any/main/userscheck > /dev/null 2>&1
 chmod 777 chany > /dev/null 2>&1
 chmod 777 userscheck > /dev/null 2>&1
 clear
 mkdir /usr/lib/checkany > /dev/null 2>&1
 cd /usr/lib/checkany || exit
 rm checkany.py > /dev/null 2>&1
-wget https://raw.githubusercontent.com/LightY4gami/checkany/main/checkany.py > /dev/null 2>&1
+wget https://raw.githubusercontent.com/RPful/Any/main/checkany.py > /dev/null 2>&1
 chmod 777 checkany.py > /dev/null 2>&1
 clear
 echo -e "        \033[1;33m • \033[1;32mINSTALAÇÃO CONCLUÍDA\033[1;33m • \033[0m"
@@ -33,6 +33,6 @@ clear
 echo ""
 echo -e "\033[1;31m \033[1;33mCOMANDO PRINCIPAL: \033[1;32mchany\033[0m"
 echo ""
-echo -e "\033[1;33m MAIS INFORMAÇÕES \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@anyvpnmodgroup\033[0m"
+echo -e "\033[1;33m MAIS INFORMAÇÕES \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@RP4321\033[0m"
 cat /dev/null > ~/.bash_history && history -c
 exit
